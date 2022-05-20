d7fbec6b2ac8f80597499efc9ffa00b6fc2bd397
Savills C6JGV93 <briceto102@gmail.com>
2022-05-20 18:27:24 +0700

    Ex1,Ex2

---
+++ appium/Java/FirstJava/src/Lab2/Ex1/BMICalculate.java
@@ -0,0 +1,34 @@
+package Lab2.Ex1;
+
+import java.util.Scanner;
+
+public class BMICalculate {
+
+    public static void main(String[] args) {
+        //BMI = weight / (height*2)
+
+        Scanner usrInput = new Scanner(System.in);
+        System.out.println("Please input weight (kg): ");
+        double weightValue = usrInput.nextDouble();
+
+        System.out.println("Please input height (meter):");
+        double heightValue =usrInput.nextDouble();
+
+        double result = weightValue / (heightValue * heightValue);
+        if(result <18.5){
+            System.out.println("Underweight");
+            System.out.println("You should eat more to gain weight.Good luck");
+        } else if (result >= 18.5 && result <= 24.9) {
+            System.out.println("Normal weight");
+            System.out.println("Congratulation! Continue with your plan, don't forget to do exercise everyday");
+        } else if (result>= 25 && result <=29.9) {
+            System.out.println("Overweight");
+            System.out.println("Warning!!!! Stop eating sugar food, change your habit and do more exercise.");
+        }
+        else {
+            System.out.println("Obesity");
+            System.out.println(".................-----> Diet or Die................");
+          ;
+        }
+    }
+}
---
+++ appium/Java/FirstJava/src/Lab2/Ex2/EvenOROdd.java
@@ -0,0 +1,26 @@
+package Lab2.Ex2;
+
+import java.util.Scanner;
+
+public class EvenOROdd {
+
+    public static void main(String[] args) {
+        final boolean MUST_INTEGER_VALUE = true;
+        Scanner userinput = new Scanner(System.in);
+        System.out.println("Please input a number ");
+        boolean isIntegerValue = userinput.hasNextInt() == MUST_INTEGER_VALUE;
+        int checkNumb = userinput.nextInt();
+
+        if (isIntegerValue) {
+            if (checkNumb%2 == 0) {
+                System.out.println("This is an Even number");
+            } else {
+                System.out.println("This is an Odd number");
+            }
+        } else {
+            System.out.println("please input an integer value");
+        }
+
+
+    }
+}
